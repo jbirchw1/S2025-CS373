954ae83dc149f76047fd9c0004c9d34b75670d93
jbirchw1 <jbirchw1@binghamton.edu>
2025-02-07 12:02:09 -0500

    transitions and moving large memory to the heap

--- p1/src/main.cpp
+++ p1/src/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iterator>
 #include <sstream>
+#include <unordered_map>
 
 // Custom State class
 class State{
@@ -19,8 +20,11 @@ class State{
         bool is_accept;
 };
 
-State states[1001];
-int start_state;
+std::unordered_map<int, State> states;
+State start_state;
+//                  p                       x                 q 
+std::unordered_map<int, std::unordered_map<char, std::vector<int>>> transitions;
+// e.g., transitions[4]['a'] => {6, 7, 8, ...}
 
 void initialize_states() {
     for(int i = 0; i < 1002; i++) {
@@ -28,9 +32,26 @@ void initialize_states() {
     }
 }
 
+// for office use only
 void print_states(int index) {
+    std::cout << "Start state is " << start_state.num << std::endl;
     for(int i = 0; i <= index; i++) {
         std::cout << "State " << states[i].num << " is " << ((states[i].is_accept) ? "" : "not ") << "an accept state" << std::endl;
+    }
+}
+
+void print_transitions() {
+    for (const auto& [state, symbol_map] : transitions) {
+        for (const auto& [symbol, next_states] : symbol_map) {
+            std::cout << "Transition: " << state << " --(" << symbol << ")--> {";
+            
+            for (size_t i = 0; i < next_states.size(); i++) {
+                std::cout << next_states[i];
+                if (i < next_states.size() - 1) std::cout << ", ";
+            }
+            
+            std::cout << "}" << std::endl;
+        }
     }
 }
 
@@ -45,13 +66,28 @@ void parse(char* filename) {
         while(std::getline(iss, token, '\t'))
             tokens.push_back(token);
         // update state in array of states with this information
-        if(tokens.at(0).compare("state") == 0) {
-            if(tokens.at(2).find("accept") != std::string::npos) {
+        if(tokens.at(0).compare("state") == 0) { // state input
+            if(tokens.size() > 3) {
+              if((tokens.at(2).find("accept") != std::string::npos) || (tokens.at(3).find("accept") != std::string::npos)) {
                 states[std::stoi(tokens.at(1))].is_accept = true;
             }
-            if(tokens.at(2).find("start") != std::string::npos) {
-                start_state = std::stoi(tokens.at(1));
+            if((tokens.at(2).find("start") != std::string::npos) || (tokens.at(3).find("start") != std::string::npos)) {
+                start_state = states[std::stoi(tokens.at(1))];
+            }  
             }
+            else {
+                if(tokens.at(2).find("accept") != std::string::npos) {
+                    states[std::stoi(tokens.at(1))].is_accept = true;
+                }
+                if(tokens.at(2).find("start") != std::string::npos) {
+                    start_state = states[std::stoi(tokens.at(1))];
+                }
+            }
+            
+        }
+        if(tokens.at(0).compare("transition") == 0) { // transition input
+            // ! NOTE: tokens.at(2).at(0) may only work if there is a char there to begin with
+            transitions[std::stoi(tokens.at(1))][tokens.at(2).at(0)].push_back(std::stoi(tokens.at(3)));
         }
         tokens.clear();
     }
@@ -61,5 +97,6 @@ int main(int argc, char *argv[]) {
     initialize_states();
     parse(argv[1]);
     print_states(7);
+    print_transitions();
     return 0;
 }
